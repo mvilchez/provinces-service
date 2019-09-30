8515ad5c9448a114965f626bc241304492ee49a4
jrivert <jrivert@AT-9JKX7H2.atsistemas.lan>
2019-09-30 17:02:50 +0200

    cambios en import

--- src/main/java/es/sinvied/provinces/service/ProvincesService.java
+++ src/main/java/es/sinvied/provinces/service/ProvincesService.java
@@ -2,7 +2,6 @@ package es.sinvied.provinces.service;
 
 import es.sinvied.provinces.domain.ProvinceDTO;
 import es.sinvied.provinces.domain.ProvinceListDTO;
-import org.springframework.stereotype.Service;
 
 public interface ProvincesService {
     ProvinceListDTO getProvinces();
